9f30077bfa045ec78442efcece3da1d2e807a495
Illuminatus66 <varunsodhani123@gmail.com>
2024-10-22 07:17:28 +0530

    adding devtools

--- src/store.js
+++ src/store.js
@@ -8,6 +8,6 @@ const rootReducer = combineReducers({
   products: productReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() , applyMiddleware(thunk));
 
 export default store;
