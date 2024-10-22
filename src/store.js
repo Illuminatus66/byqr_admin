65ce5cd98b5ad8d65755bf65d7fd1941b2bf64f3
Illuminatus66 <varunsodhani123@gmail.com>
2024-10-22 07:26:51 +0530

    some changes

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import { thunk } from "redux-thunk";
 import authReducer from "./reducers/auth";
 import productReducer from "./reducers/products";
@@ -7,7 +7,7 @@ const rootReducer = combineReducers({
   auth: authReducer,
   products: productReducer,
 });
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() , applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
