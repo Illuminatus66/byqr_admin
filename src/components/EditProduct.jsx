891c7f906734bcd505000a3cf43c0b758cd3ab2a
Illuminatus66 <varunsodhani123@gmail.com>
2024-10-22 07:36:49 +0530

    changes in the way product editing works

--- src/components/EditProduct.jsx
+++ src/components/EditProduct.jsx
@@ -45,110 +45,114 @@ const EditProduct = () => {
 
   return (
     <div className="edit-product-container">
-      <h2 className="edit-product-title">Edit Product</h2>
-      <form onSubmit={handleSubmit} className="edit-product-form">
-        <div className="form-group">
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={productData.name}
-            onChange={handleChange}
-            required
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label>Price:</label>
-          <input
-            type="number"
-            name="price"
-            value={productData.price}
-            onChange={handleChange}
-            required
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label>Stock:</label>
-          <input
-            type="number"
-            name="stock"
-            value={productData.stock}
-            onChange={handleChange}
-            required
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label>Category:</label>
-          <input
-            type="text"
-            name="category"
-            value={productData.category}
-            onChange={handleChange}
-            required
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label>Description:</label>
-          <textarea
-            name="description"
-            value={productData.description}
-            onChange={handleChange}
-            required
-            className="form-textarea"
-          />
-        </div>
+      {productData ? (
+        <form onSubmit={handleSubmit} className="edit-product-form">
+          <h2 className="edit-product-title">Edit Product</h2>
+          <div className="form-group">
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={productData.name}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Price:</label>
+            <input
+              type="number"
+              name="price"
+              value={productData.price}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Stock:</label>
+            <input
+              type="number"
+              name="stock"
+              value={productData.stock}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Category:</label>
+            <input
+              type="text"
+              name="category"
+              value={productData.category}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Description:</label>
+            <textarea
+              name="description"
+              value={productData.description}
+              onChange={handleChange}
+              required
+              className="form-textarea"
+            />
+          </div>
 
-        {/* Thumbnail URL with live preview */}
-        <div className="form-group">
-          <label>Thumbnail URL:</label>
-          <input
-            type="text"
-            name="thumbnail"
-            value={productData.thumbnail}
-            onChange={handleChange}
-            required
-            className="form-input"
-          />
-          <img
-            src={
-              isValidImageURL(productData.thumbnail)
-                ? productData.thumbnail
-                : placeholderImage
-            }
-            alt="Thumbnail Preview"
-            className="image-preview"
-          />
-        </div>
+          {/* Thumbnail URL with live preview */}
+          <div className="form-group">
+            <label>Thumbnail URL:</label>
+            <input
+              type="text"
+              name="thumbnail"
+              value={productData.thumbnail}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+            <img
+              src={
+                isValidImageURL(productData.thumbnail)
+                  ? productData.thumbnail
+                  : placeholderImage
+              }
+              alt="Thumbnail Preview"
+              className="image-preview"
+            />
+          </div>
 
-        {/* Images array with live previews */}
-        <div className="form-group">
-          <label>Images:</label>
-          {productData.imgs.map((img, index) => (
-            <div key={index} className="image-group">
-              <input
-                type="text"
-                value={img}
-                onChange={(e) => handleImageChange(index, e.target.value)}
-                className="form-input"
-              />
-              <img
-                src={isValidImageURL(img) ? img : placeholderImage}
-                alt={`Number ${index + 1} Preview`}
-                className="image-preview"
-              />
-            </div>
-          ))}
-        </div>
+          {/* Images array with live previews */}
+          <div className="form-group">
+            <label>Images:</label>
+            {productData.imgs.map((img, index) => (
+              <div key={index} className="image-group">
+                <input
+                  type="text"
+                  value={img}
+                  onChange={(e) => handleImageChange(index, e.target.value)}
+                  className="form-input"
+                />
+                <img
+                  src={isValidImageURL(img) ? img : placeholderImage}
+                  alt={`Number ${index + 1} Preview`}
+                  className="image-preview"
+                />
+              </div>
+            ))}
+          </div>
 
-        {/* Submit button */}
-        <button type="submit" className="submit-button">
-          Update Product
-        </button>
-      </form>
+          {/* Submit button */}
+          <button type="submit" className="submit-button">
+            Update Product
+          </button>
+        </form>
+      ) : (
+        <p>Loading product data...</p>
+      )}
     </div>
   );
 };
